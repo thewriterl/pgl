7044ad54bd0cf2490ac1f154cdc7d4a2809905de
luizao <luiz.darthvader@gmail.com>
2019-09-29 11:36:58 -0300

    Initial Commit

---
+++ pgl2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pgl2
+//
+//  Created by Luiz Fernando França on 29/09/19.
+//  Copyright © 2019 Luiz Fernando França. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pgl2Tests/pgl2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  pgl2Tests.swift
+//  pgl2Tests
+//
+//  Created by Luiz Fernando França on 29/09/19.
+//  Copyright © 2019 Luiz Fernando França. All rights reserved.
+//
+
+import XCTest
+@testable import pgl2
+
+class pgl2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
