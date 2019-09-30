1b798dee7731e99fe70c811a130c6222a9e36ed6
luizao <luiz.darthvader@gmail.com>
2019-09-30 01:11:44 -0300

    esse ta top

--- pgl2/ViewController.swift
+++ pgl2/ViewController.swift
@@ -10,19 +10,98 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let hello: UILabel = {
-        let hello = UILabel()
-        hello.text = "Hello"
-        return hello
+    let cardView: UIView = {
+        let card = UIView()
+        card.backgroundColor = .red
+        card.layer.cornerRadius = 25
+        return card
     }()
+    
+    let cardNameLabel: UILabel = {
+        let name = UILabel()
+        name.text = "Pagglo Payseed"
+        name.textColor = .white
+        return name
+    }()
+    
+    let cardBackground: UIView = {
+        let background = UIView()
+        background.backgroundColor = .black
+        return background
+    }()
+        
+    let funcionalidadesCV = UICollectionView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), collectionViewLayout: UICollectionViewFlowLayout())
 
+    override func viewDidLayoutSubviews() {
+        self.funcionalidadesCV.contentSize = self.funcionalidadesCV.frame.size;
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.addSubview(hello)
+        
+        funcionalidadesCV.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellID")
+        funcionalidadesCV.backgroundColor = .yellow
+        funcionalidadesCV.delegate = self
+        funcionalidadesCV.dataSource = self
+        
+        view.addSubview(cardBackground)
+        view.addSubview(funcionalidadesCV)
+        
+        cardBackground.addSubview(cardView)
+        
+        cardView.addSubview(cardNameLabel)
+        
+        cardBackground.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.centerYAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        cardView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view?.centerYAnchor, right: view.rightAnchor, paddingTop: view.safeAreaInsets.top + 50, paddingLeft: 20, paddingBottom: 20, paddingRight: 20, width: 200, height: 100)
+        
+        cardNameLabel.anchor(top: nil, left: cardView.leftAnchor, bottom: cardView.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 20, paddingBottom: 20, paddingRight: 0, width: 0, height: 0)
+        
+        
+        funcionalidadesCV.anchor(top: cardBackground.bottomAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        
         
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 4
+        } else {
+            return 7
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath)
+        if indexPath.section == 0 {
+            cell.backgroundColor = .purple
+        } else {
+            cell.backgroundColor = .blue
+        }
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        switch indexPath.section {
+            case 0:
+                return CGSize(width: view.frame.width / 5, height: 100)
+            case 1:
+                return CGSize(width: view.frame.width / 5, height: 200)
+            default:
+                return CGSize(width: view.frame.width / 5, height: 25)
+        }
+        
+    }
+    
+}
